4c36206f5a247e08260b889865e3ae761def73f8
LukeChuang123 <zhuang890212@gmail.com>
2019-12-19 13:46:59 +0800

    將攻守成績以寬型資料整合進each_game_data表格

---
+++ CPBL Analysis/test.py
@@ -0,0 +1,13 @@
+# string = '001'
+# print(''.join([s for s in string if s != '0']))
+
+# for a,b in zip([1,2,3],[4,5,6]):
+#     print(zip([1,2,3],[4,5,6]))
+#     print(a,b)
+#     # print(a)
+#     # print(b)
+
+a = [1,2,3]
+b = [4,5,6]
+x,y = a[0],b[2]
+print(x,y)
